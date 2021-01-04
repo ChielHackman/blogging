5fc55f9921c3e17f23a51fc3ab3ca02c62db73b2
Chiel Hackman <chielhackman@gmail.com>
2021-01-04 13:54:48 +0100

    Added specs for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Is a valid post' do
+    post = Post.new
+    expect(post.valid?).to eq(false)
+  end
+
+  it 'Is a invalid post' do
+    post = Post.new(title: 'Title', body: 'Body')
+    expect(post.valid?).to eq(true)
+  end
 end
